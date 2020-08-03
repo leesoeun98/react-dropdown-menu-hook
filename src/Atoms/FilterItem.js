6e9b324274d6da8f9cd33f87626eda13a64c793f
soeun lee <thdmsdl87@gmail.com>
2020-08-03 15:18:48 +0900

    Add all css (styled-components format)

---
+++ src/Atoms/FilterItem.js
@@ -0,0 +1,11 @@
+import styled, {css} from "styled-components";
+
+export default styled.div`
+    width: 6rem;
+    display: flex;
+    flex-direction: row;
+    height: fit-content;
+    background: #ffffff;
+    justify-content: left;
+    align-items: center;
+    `;
---
+++ src/Atoms/FilterWrapper.js
@@ -0,0 +1,12 @@
+import styled, {css} from "styled-components";
+
+export default styled.div`
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    height: fit-content;
+    background: #ffffff;
+    justify-content: left;
+    align-items: center;
+    padding: 0 0 0 2rem;
+    `;
