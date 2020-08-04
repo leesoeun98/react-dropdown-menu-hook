ef28a823969ec27971af6ed3a4e8d666a68d3165
soeun lee <thdmsdl87@gmail.com>
2020-08-04 11:26:24 +0900

    Add another code to change title of dropdown menu

--- src/Molecules/Filter.js
+++ src/Molecules/Filter.js
@@ -3,23 +3,26 @@ import FilterButton from "../Molecules/FilterButton";
 import DropdownMenu from "../Molecules/DropdownMenu";
 import styled from "styled-components";
 import React, { Component } from "react";
-import {useState} from 'react';
+import { useState } from "react";
 
 export default function Filter(props) {
-    const [clicked, setClicked]=useState(false);
-    const openMenu=()=>{
-        console.log(clicked)
-        setClicked(!clicked)
-	}
-	const [item, setItem]=useState(null);
+	const [clicked, setClicked] = useState(false);
+	const openMenu = () => {
+		console.log(clicked);
+		setClicked(!clicked);
+	};
+	const [item, setItem] = useState(null);
 
-	const selectItem=(item)=>{
-		console.log(item)
-		setItem(item)
-	}
+	const selectItem = (item) => {
+		console.log(item);
+		setItem(item);
+	};
 	return (
-		<FilterWrapper onClick={()=>openMenu()}>
-			<FilterButton title={item? item.name : props.title}></FilterButton>
+		<FilterWrapper onClick={() => openMenu()}>
+			<FilterButton
+				title={item ? item.name : props.title}
+				//title = {item?.name || props.title}
+			></FilterButton>
 			{clicked && <DropdownMenu data={props.data} selectItem={selectItem}></DropdownMenu>}
 		</FilterWrapper>
 	);
